1b88afc18b05ce7905ed51203e60bfe30c52ce54
Simon Ordish <simon.ordish@masagi.co.uk>
2023-05-10 12:32:28 +0200

    Change context values

--- VerifySignature2.go
+++ VerifySignature2.go
@@ -18,7 +18,7 @@ func cBuf(goSlice []byte) *C.uchar {
 // VerifySignature verifies that the given signature for the given message was signed by the given public key.
 func VerifySignature2(message []byte, signature []byte, publicKey []byte) bool {
 	// Create a secp256k1 context
-	ctx := C.secp256k1_context_create(C.SECP256K1_CONTEXT_NONE)
+	ctx := C.secp256k1_context_create(C.SECP256K1_CONTEXT_SIGN | C.SECP256K1_CONTEXT_VERIFY)
 
 	// Allocate memory for the message, signature, and public key
 	cMessage := cBuf(message)
